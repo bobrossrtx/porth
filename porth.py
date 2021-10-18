9c8311cad70aaf2b163ef6bc9e3f2d2f97670d1d
bobrossrtx <bobrossrtx@gmail.com>
2021-10-18 03:17:00 +0100

    feat: compile hardcoded program to native assembly + dumping values

--- porth.py
+++ porth.py
@@ -4,6 +4,7 @@
 import sys
 import subprocess
 
+debug = True
 iota_counter = 0
 
 
@@ -63,8 +64,66 @@ def simulate_program(program):
 def compile_program(program, out_file_path):
     with open(out_file_path, "w") as out:
         out.write("segment .text\n")
+        out.write("dump:\n")
+        out.write("    mov     r9, -3689348814741910323\n")
+        out.write("    sub     rsp, 40\n")
+        out.write("    mov     BYTE [rsp+31], 10\n")
+        out.write("    lea     rcx, [rsp+30]\n")
+        out.write(".L2:\n")
+        out.write("    mov     rax, rdi\n")
+        out.write("    lea     r8, [rsp+32]\n")
+        out.write("    mul     r9\n")
+        out.write("    mov     rax, rdi\n")
+        out.write("    sub     r8, rcx\n")
+        out.write("    shr     rdx, 3\n")
+        out.write("    lea     rsi, [rdx+rdx*4]\n")
+        out.write("    add     rsi, rsi\n")
+        out.write("    sub     rax, rsi\n")
+        out.write("    add     eax, 48\n")
+        out.write("    mov     BYTE [rcx], al\n")
+        out.write("    mov     rax, rdi\n")
+        out.write("    mov     rdi, rdx\n")
+        out.write("    mov     rdx, rcx\n")
+        out.write("    sub     rcx, 1\n")
+        out.write("    cmp     rax, 9\n")
+        out.write("    ja      .L2\n")
+        out.write("    lea     rax, [rsp+32]\n")
+        out.write("    mov     edi, 1\n")
+        out.write("    sub     rdx, rax\n")
+        out.write("    xor     eax, eax\n")
+        out.write("    lea     rsi, [rsp+32+rdx]\n")
+        out.write("    mov     rdx, r8\n")
+        out.write("    mov     rax, 1\n")
+        out.write("    syscall\n")
+        out.write("    add     rsp, 40\n")
+        out.write("    ret\n")
         out.write("global _start\n")
         out.write("_start:\n")
+        for op in program:
+            assert COUNT_OPS == 4, "Exhaustive handling of operations in compilation"
+            if op[0] == OP_PUSH:
+                out.write("    ;; push %d\n" % op[1])
+                out.write("    push %d\n" % op[1])
+            elif op[0] == OP_PLUS:
+                out.write("    ;; plus\n")
+                out.write("    pop rax\n")
+                out.write("    pop rbx\n")
+                out.write("    add rax, rbx\n")
+                out.write("    push rax\n")
+            elif op[0] == OP_MINUS:
+                out.write("    ;; minus\n")
+                out.write("    pop rax\n")
+                out.write("    pop rbx\n")
+                out.write("    sub rbx, rax\n")
+                out.write("    push rbx\n")
+            elif op[0] == OP_DUMP:
+                out.write("    ;; dump\n")
+                out.write("    pop rdi\n")
+                out.write("    call dump\n")
+            else:
+                assert False, "unreachable"
+
+        # Exit out of program
         out.write("    mov rax, 60\n")
         out.write("    mov rdi, 0\n")
         out.write("    syscall\n")
@@ -92,6 +151,12 @@ def usage():
     print()
 
 
+def call_cmd(cmd):
+    if debug:
+        print(cmd)
+    subprocess.call(cmd)
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         usage()
@@ -107,7 +172,7 @@ if __name__ == "__main__":
         simulate_program(program)
     elif subcommand == "com":
         compile_program(program, "output.asm")
-        subprocess.call(["nasm", "-felf64", "output.asm"])
-        subprocess.call(["ld", "-o", "output", "output.o"])
+        call_cmd(["nasm", "-felf64", "output.asm"])
+        call_cmd(["ld", "-o", "output", "output.o"])
     else:
         print("Error: unknown subcommand")
